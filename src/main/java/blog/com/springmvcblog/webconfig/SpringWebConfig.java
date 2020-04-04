f47b4998733c27ff812c1d03df59a101be7f75fa
an <nguyentruongan941@gmail.com>
2020-04-04 13:21:46 +0700

    initial commit

---
+++ src/main/java/blog/com/springmvcblog/webconfig/SpringWebConfig.java
@@ -0,0 +1,21 @@
+package blog.com.springmvcblog.webconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc // mvc:annotation-driven
+@Configuration
+@ComponentScan({ "blog.com.springmvcblog" })
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
